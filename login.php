956142ac393336960a08a09fa644b5d478ef1052
Riminamina <khairotq@gmail.com>
2024-11-20 09:56:39 +0700

    update login.php dan css

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <form method="POST" action="">
+            <h2>Login Your Account</h2>
+            <label>Username</label><br>
+            <input type="text" name="username" required><br><br>
+            <label>Password</label><br>
+            <input type="password" name="password" required><br><br>
+            <input type="button" name="submit" value="Login"><br>
+            Belum Punya Akun? <a href="">Registerasi</a>
+        </form>
+    </div>
+</body>
+</html>
