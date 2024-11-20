789333f1a116a0baad2cd85bbcbd453738729a94
Riminamina <khairotq@gmail.com>
2024-11-20 14:49:03 +0700

    login

--- login.php
+++ login.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
 <body>
+    <img src="asset/icon.png" class="icon">
     <div class="container">
+        <h2>Login Your Account</h2>
+        <section class="login-section">
         <form method="POST" action="">
-            <h2>Login Your Account</h2>
-            <label>Username</label><br>
+            <label><i class="fas fa-user"></i>Username</label><br>
             <input type="text" name="username" required><br><br>
             <label>Password</label><br>
             <input type="password" name="password" required><br><br>
-            <input type="button" name="submit" value="Login"><br>
-            Belum Punya Akun? <a href="">Registerasi</a>
-        </form>
+            <input type="checkbox" name="remember_me">
+            <label for="remember_me">Remember me</label><br><br>
+            <input type="submit" name="submit" value="Login"><br>
+        </section>
+        <p>Belum Punya Akun? <a href="">Registerasi</a></p>
     </div>
 </body>
 </html>
